1329ecb4f7870271cdf738acd7615c8ffbe2beaf
App-Designer2 <53442622+App-Designer2@users.noreply.github.com>
2021-03-22 17:55:08 +0100

    Update MyDesign.swift

--- MyDesign.swift
+++ MyDesign.swift
@@ -290,14 +290,3 @@ struct MyDesign_Previews: PreviewProvider {
     }
 }
 
-/*struct VisualEffectView: UIViewRepresentable {
-    let style: UIBlurEffect.Style
-    
-    func makeUIView(context: Context) -> UIVisualEffectView {
-        return UIVisualEffectView(effect: UIBlurEffect(style: style))
-    }
-    
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-        uiView.effect = UIBlurEffect(style: style)
-    }
-}*/
